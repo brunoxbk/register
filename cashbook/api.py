de14c6783ee8c026c071cfdfb73649e980636cd9
Wesley Bruno Alves Batista <wesley.batista@aguasdeteresina.com.br>
2021-04-26 18:06:56 -0300

    filter user

--- cashbook/api.py
+++ cashbook/api.py
@@ -10,6 +10,9 @@ class MovementList(generics.ListCreateAPIView):
     queryset = Movement.objects.all()
     serializer_class = MovementSerializer
 
+    def get_queryset(self, **kwargs):
+        return Movement.objects.filter(user=self.request.user)
+
 
 class MovementDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Movement.objects.all()
